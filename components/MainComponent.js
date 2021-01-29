e4afd1228ee44312057fce366dfc454593884e95
Caleb Jones <calebj516@gmail.com>
2021-01-28 21:10:56 -0600

    Navigation Part 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
-import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { View, Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 
 const DirectoryNavigator = createStackNavigator(
@@ -18,14 +19,41 @@ const DirectoryNavigator = createStackNavigator(
         backgroundColor: "#5637DD",
       },
       headerTintColor: "#fff",
-      headerTintStyle: {
+      headerTitleStyle: {
         color: "#fff",
       },
     },
   }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+  },
+  {
+    drawerBackgroundColor: "#CEC8FF",
+  }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
   render() {
